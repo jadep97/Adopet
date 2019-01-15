f07493db523beddf16c9028f638b9c3d3c5270c6
Jade <jadepaypa@gmail.com>
2019-01-15 15:33:52 +0800

    pet table added

---
+++ database/migrations/2019_01_15_060111_create_pets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->increments('id')->primarykey();
+            $table->string('petName');
+            $table->string('petOwner');
+            $table->string('breed');
+            $table->string('petBirth');
+            $table->string('address');
+            $table->string('petInfo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pets');
+    }
+}
